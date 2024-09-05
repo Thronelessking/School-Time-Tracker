5519c814519ec0793b8447be633dbba537fc210a
George Stevenson <gstevenson@gboe.org>
2024-09-05 14:51:25 -0400

    setting up javascript variables to handle scheduling

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const one = ["8:45", "9:39"];
+const two = ["9:41", "10:35"];
+const hr = ["10:37", "10:49"];
+const three = ["10:51", "11::45"];
+const four = ["11:47", "1:13"];
+const five = ["1:15", "2:09"];
+const six = ["2:11", "3:05"];
+
+const date = new Date();
+
+console.log(date)
